6254a0c53a937ae26086f1edca8f0cbfec4f446e
Manjupriyakannan <83221948+manjupriyak@users.noreply.github.com>
2025-11-08 23:34:45 +0530

    Add files via upload
    First week Assignments

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,18 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+
+		float version =91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		System.out.println("version of type float with a value of "+version);   
+		System.out.println("developer of type String with a value of "+developer);
+		System.out.println("isBeta of type booleanwith a value of "+isBeta); 
+		System.out.println("releaseYear of type int with a value of "+releaseYear);   
+		 System.out.println("shortcutKey of type charwith a value of "+shortcutKey);
+	}
+
+}
